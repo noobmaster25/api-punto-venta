39372225a75c269f6c3598bc179c59a591e46215
noobmaster25 <sosafloresluisangel@gmail.com>
2023-05-23 18:21:00 -0600

    agregando configuracion de cors

--- src/main/java/com/example/apipuntoventa/config/WebSecurityConfig.java
+++ src/main/java/com/example/apipuntoventa/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.apipuntoventa.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.example.apipuntoventa.security.JwtAuthenticationEntryPoint;
 import com.example.apipuntoventa.security.JwtAuthenticationFilter;
@@ -59,11 +64,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		.authorizeRequests().antMatchers("/punto-venta-api/v0/auth/**").permitAll()
 		.anyRequest().authenticated()
 		.and()
+		.cors().and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		;
 	
 		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 	}
+	@Bean
+	  public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("*"));
+	    configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+	    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
+	  }
 
 	
 }
